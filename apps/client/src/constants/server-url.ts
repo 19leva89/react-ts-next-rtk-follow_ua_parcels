2f491db6f920f9792af6f91a054a0311a07997f3
Dmitry Soboliev <19leva89@gmail.com>
2025-11-07 09:16:12 +0200

    upd SERVER_URL

--- apps/client/src/constants/server-url.ts
+++
@@ -1,4 +0,0 @@
-export const SERVER_URL =
-	process.env.NODE_ENV === 'production'
-		? 'https://react-ts-follow-ua-parcels-server.onrender.com'
-		: 'http://localhost:8000'
--- apps/client/src/services/api.ts
+++ apps/client/src/services/api.ts
@@ -1,10 +1,9 @@
 import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react'
 
 import { RootState } from '../app/store'
-import { SERVER_URL } from '@/constants/server-url'
 
 const baseQuery = fetchBaseQuery({
-	baseUrl: `${SERVER_URL}/api`,
+	baseUrl: `${process.env.NEXT_PUBLIC_SERVER_URL}/api`,
 	prepareHeaders: (headers, { getState }) => {
 		const token = (getState() as RootState).auth.user?.token || localStorage.getItem('token')
 
